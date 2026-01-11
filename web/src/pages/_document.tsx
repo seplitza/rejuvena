53614c4a55bd5d42d42a2af9f4ffa193cc3564d2
seplitza <seplitza@gmail.com>
2026-01-11 09:59:07 +0300

    Fix favicon paths: add explicit basePath for production

--- web/src/pages/_document.tsx
+++ web/src/pages/_document.tsx
@@ -1,15 +1,18 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
+  // Get basePath from environment or use default
+  const basePath = process.env.NODE_ENV === 'production' ? '/rejuvena' : '';
+  
   return (
     <Html lang="ru">
       <Head>
         <meta name="description" content="Rejuvena - Омоложение лица естественным путем" />
         
         {/* Favicons */}
-        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-        <link rel="alternate icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.svg" />
+        <link rel="icon" type="image/svg+xml" href={`${basePath}/favicon.svg`} />
+        <link rel="alternate icon" href={`${basePath}/favicon.ico`} />
+        <link rel="apple-touch-icon" href={`${basePath}/apple-touch-icon.svg`} />
         <meta name="theme-color" content="#B794F6" />
         
         {/* Telegram Web App API */}
@@ -20,11 +23,16 @@ export default function Document() {
           dangerouslySetInnerHTML={{
             __html: `
               (function() {
-                // Handle redirect from 404.html
-                var redirect = sessionStorage.getItem('r                var redirect = sessionStorage.getItem('r                var redireItem('redirect');
+                var redirect = sessionStorage.getItem('redirect');
+                if (redirect) {
+                  sessionStorage.removeItem('redirect');
                   history.replaceState(null, '', redirect);
                 }
-                                 // H          -ba                                 // H          -ba                                 // H          -ba                             var                     ion.                                 // H          -ba              route                                 // )();
+                if (window.location.hash && window.location.hash.startsWith('#/')) {
+                  var route = window.location.hash.slice(1);
+                  history.replaceState(null, '', route);
+                }
+              })();
             `,
           }}
         />
